5c17764030dc22453ece2a141042b70f0f836481
Matheus Carvalho <matheus.carvalho@modal.com.br>
2022-03-07 11:20:24 -0300

    ensure RemoteAddAccount completes with error when HttpClient complete with error

--- Data/UseCases/RemoteAddAccount.swift
+++ Data/UseCases/RemoteAddAccount.swift
@@ -3,15 +3,16 @@ import Domain
 
 public final class RemoteAddAccount {
     private let url: URL
-    private let httpPostClient: HttpPostClient
+    private let httpClient: HttpPostClient
     
     public init(url: URL, httpPostClient: HttpPostClient) {
         self.url = url
-        self.httpPostClient = httpPostClient
+        self.httpClient = httpPostClient
     }
     
-    public func add(addAccountModel: AddAccountModel) {
-        httpPostClient.post(to: url, with: addAccountModel.toData())
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (DomainError) -> Void) {
+        httpClient.post(to: url, with: addAccountModel.toData()) { error in
+            completion(.unexpected)
+        }
     }
-    
 }
