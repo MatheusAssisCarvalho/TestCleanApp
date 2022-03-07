36eb4f14606fd6ecf9f1c556ae9e2877d64e696c
Matheus Carvalho <matheus.carvalho@modal.com.br>
2022-03-07 10:22:42 -0300

    ensure remote add account calls httpCliente only ones

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -9,7 +9,7 @@ class RemoteAddAccountTests: XCTestCase {
         let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel: makeAddAcountModel())
         
-        XCTAssertEqual(httpClientSpy.url, url)
+        XCTAssertEqual(httpClientSpy.urls, [url])
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
@@ -33,11 +33,11 @@ extension RemoteAddAccountTests {
         return AddAccountModel(name: "Nome legal", email: "emailLegal@estudo.com", password: "22930293", passwordConfirmation: "22930293")
     }
     class HttpClientSpy: HttpPostClient {
-        var url: URL?
+        var urls = [URL]()
         var data: Data?
         
         func post(to url: URL, with data: Data?) {
-            self.url = url
+            self.urls.append(url)
             self.data = data
         }
     }
