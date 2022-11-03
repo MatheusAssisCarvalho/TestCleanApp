98d76c87a816137e9108a8e58198b96fa680ebac
Matheus Carvalho <matheus.assis022@gmail.com>
2022-11-03 13:51:43 -0300

    ajustando arquivos

--- DataTests/Helpers/TestExtensions.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-extension XCTestCase {
-    func checkMemoryLeak (for instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, file: file, line: line)
-        }
-    }
-}
--- Domain/Errors/DominError.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public enum DomainError: Error {
-    case unexpected
-}
